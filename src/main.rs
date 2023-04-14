c3f65bc05dc2d2e364b4d67dbc3968b9e1f854b6
AllenYolk <allen.yfhuang@gmail.com>
2023-04-14 23:04:05 +0800

    feat: add CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(next_line_help = true)]
+struct Cli {
+    query: Option<String>,
+
+    #[arg(short, long)]
+    #[arg(value_enum)]
+    #[arg(default_value_t = Website::Dblp)]
+    website: Website,
+
+    #[arg(short, long)]
+    #[arg(default_value_t = 1)]
+    n_considered: u32,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, ValueEnum)]
+enum Website {
+    Dblp,
+}
+
 fn main() {
-    let url = "https://dblp.org";
-    let response = reqwest::blocking::get(url).unwrap();
-    let body = response.text().unwrap();
-    println!("{}", body);
+    let args = Cli::parse();
+    dbg!(args);
 }
